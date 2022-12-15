390d8d66c2ec6828ba6e1a17ce4f823a3c50b368
Gourav Saxena <gourav.saxena@kaltura.com>
2022-12-15 12:30:45 +0530

    - Cleanup

--- .github/publish.sh
+++ .github/publish.sh
@@ -1,27 +1,30 @@
 checkout() {
-
     echo Checking out newtag = $NEW_TAG, release type = $RELEASE_TYPE
 
-                case $RELEASE_TYPE in
+    case $RELEASE_TYPE in
     Full)
-                git checkout -b "$BRANCH_NAME"
-    ;;
+        git checkout -b "$BRANCH_NAME";;
     Patch)
-                git checkout "$BRANCH_NAME"
-                ;;
+        git checkout "$BRANCH_NAME";;
     Update)
-                git checkout -b "$BRANCH_NAME" $PREV_TAG
-                ;;
-                esac
+        git checkout -b "$BRANCH_NAME" $PREV_TAG;;
+    esac
 }
 
 set_version() {
     echo Setting version of $REPO_NAME to $NEW_VERSION
+
+    # Changing the version in version.gradle file
+    if [ "$COMPONENT" = "netkit" ]; then
+           perl -pi -e "s/^ext.netkitVersion.*$/ext.netkitVersion = '$NEW_VERSION'/" $NETKIT_SERVICES_VERSION_FILE
+    fi
+
+    perl -pi -e "s/^ext.netkitVersion.*$/ext.netkitVersion = '$NEW_VERSION'/" $VERSION_FILE
+    perl -pi -e "s/^ext.dtglibVersion.*$/ext.dtglibVersion = '$NEW_VERSION'/" $VERSION_FILE
+    perl -pi -e "s/^ext.playkitVersion.*$/ext.playkitVersion = '$NEW_VERSION'/" $VERSION_FILE
     perl -pi -e "s/^ext.libVersion.*$/ext.libVersion = '$NEW_VERSION'/" $VERSION_FILE
 
-    echo $(pwd)
-    echo $(ls)
-
+    # Changing the version in build.gradle file
     if [[ "$RELEASE_TYPE" = "Patch" || "$RELEASE_TYPE" = "Update" ]]; then
        echo "RELEASE_TYPE = '$RELEASE_TYPE'"
        perl -pi -e "s/playkit:playkit:$PLAYKIT_PREV_VERSION/playkit:playkit:$PLAYKIT_DEP_VERSION/" $BUILD_GRADLE
@@ -41,7 +44,6 @@ set_version() {
        perl -pi -e "s/:kaltura-player-android:develop-SNAPSHOT/.player:tvplayer:$NEW_VERSION/" $BUILD_GRADLE
        perl -pi -e "s/:playkit-dtg-android:current-SNAPSHOT/.dtg:dtglib:$DTG_DEP_VERSION/" $BUILD_GRADLE
     fi
-
 }
 
 build() {
@@ -55,13 +57,10 @@ release_and_tag() {
 
     echo Releasing version $NEW_VERSION of $REPO_NAME to GitHub
     set +e
-
     git add $VERSION_FILE
     git add $BUILD_GRADLE
     git commit -m "Update version to $NEW_TAG"
-
     set -e
-
     git push origin HEAD:$BRANCH_NAME
 
     if [[ "$RELEASE_TYPE" = "Patch" || "$RELEASE_TYPE" = "Full" ]]; then
@@ -94,18 +93,16 @@ cat << EOF > ./post.json
 }
 EOF
     fi
-                cat post.json
 
-                curl --request POST \
-                                    --url https://api.github.com/repos/GouravSna/GH-TEST/releases \
-                                    --header "authorization: Bearer $TOKEN" \
-                                    --header 'content-type: application/json' \
-                                    -d@post.json
+    cat post.json
 
-#                POST_URL=https://api.github.com/repos/GouravSna/$REPO_NAME/releases
-#
-#                curl $POST_URL -X POST -H "Accept: application/vnd.github+json" -H "Content-Type: application/json" -H "Authorization: Bearer $GITHUB_TOKEN" -d@post.json #--include
-                rm ./post.json
+    curl --request POST \
+         --url https://api.github.com/repos/GouravSna/GH-TEST/releases \
+         --header "authorization: Bearer $TOKEN" \
+         --header 'content-type: application/json' \
+         -d@post.json
+
+    rm ./post.json
 
     # delete temp branch
     #git push origin --delete $BRANCH_NAME
@@ -156,8 +153,6 @@ EOF
 
 }
 
-
-
   RELEASE_TYPE=$RELEASE_TYPE
 
   REPO_NAME=$REPO_NAME
@@ -172,13 +167,9 @@ EOF
   DTG_DEP_VERSION=$DTG_DEP_VERSION
   TOKEN=$TOKEN
   TEAMS_WEBHOOK=$TEAMS_WEBHOOK
-#
-#  NEW_TAG=$NEW_VERSION
-#  PREV_TAG=$PLAYKIT_PREV_VERSION
-#  RELEASE_URL=$REPO_URL/releases/tag/$NEW_TAG
 
-  NEW_TAG=$NEW_VERSION
-  PREV_TAG=$PLAYKIT_PREV_VERSION
+  NEW_TAG=v$NEW_VERSION
+  PREV_TAG=v$PLAYKIT_PREV_VERSION
   RELEASE_URL=$REPO_URL/releases/tag/$NEW_TAG
 
   if [[ "$RELEASE_TYPE" = "Full" || "$RELEASE_TYPE" = "Update" ]]; then
@@ -189,22 +180,22 @@ EOF
   BRANCH_NAME="patch/$NEW_TAG"
   fi
 
-  echo "BRANCH_NAME = '$BRANCH_NAME'"
-  echo "RELEASE_TYPE = '$RELEASE_TYPE'"
-  echo "REPO_NAME = '$REPO_NAME'"
-  echo "MODULE_NAME = '$MODULE_NAME'"
-  echo "VERSION_FILE = '$VERSION_FILE'"
-  echo "BUILD_GRADLE = '$BUILD_GRADLE'"
-  echo "REPO_URL = '$REPO_URL'"
-  echo "NEW_VERSION = '$NEW_VERSION'"
-  echo "PLAYKIT_PREV_VERSION = '$PLAYKIT_PREV_VERSION'"
-  echo "PLAYKIT_DEP_VERSION = '$PLAYKIT_DEP_VERSION'"
-  echo "DTG_DEP_VERSION = '$DTG_DEP_VERSION'"
-  echo "NEW_TAG = '$NEW_TAG'"
-  echo "PREV_TAG = '$PREV_TAG'"
-  echo "RELEASE_URL = '$RELEASE_URL'"
-  echo "TOKEN = '$TOKEN'"
-  echo "TEAMS_WEBHOOK = '$TEAMS_WEBHOOK'"
+#  echo "BRANCH_NAME = '$BRANCH_NAME'"
+#  echo "RELEASE_TYPE = '$RELEASE_TYPE'"
+#  echo "REPO_NAME = '$REPO_NAME'"
+#  echo "MODULE_NAME = '$MODULE_NAME'"
+#  echo "VERSION_FILE = '$VERSION_FILE'"
+#  echo "BUILD_GRADLE = '$BUILD_GRADLE'"
+#  echo "REPO_URL = '$REPO_URL'"
+#  echo "NEW_VERSION = '$NEW_VERSION'"
+#  echo "PLAYKIT_PREV_VERSION = '$PLAYKIT_PREV_VERSION'"
+#  echo "PLAYKIT_DEP_VERSION = '$PLAYKIT_DEP_VERSION'"
+#  echo "DTG_DEP_VERSION = '$DTG_DEP_VERSION'"
+#  echo "NEW_TAG = '$NEW_TAG'"
+#  echo "PREV_TAG = '$PREV_TAG'"
+#  echo "RELEASE_URL = '$RELEASE_URL'"
+#  echo "TOKEN = '$TOKEN'"
+#  echo "TEAMS_WEBHOOK = '$TEAMS_WEBHOOK'"
 
   checkout
   set_version

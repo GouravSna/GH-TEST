d220600ba91403c6ce52d0a5887fa4f9d738e9ed
Gourav Saxena <38206744+GouravSna@users.noreply.github.com>
2022-12-28 13:29:37 +0530

    Update release_notes.sh

--- .github/release_notes.sh
+++ .github/release_notes.sh
@@ -3,8 +3,7 @@ release_and_tag() {
     curl --request POST \
          --url https://api.github.com/repos/GouravSna/GH-TEST/pulls \
          --header "authorization: Bearer $TOKEN" \
-         --header 'content-type: application/json' \
-         -d@post.json
+         --header 'content-type: application/json'
 
 }
 

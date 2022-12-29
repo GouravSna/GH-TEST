c6bc36877c91add8c81aab19e3a0db59f57b8125
Gourav Saxena <gourav.saxena@kaltura.com>
2022-12-29 16:50:23 +0530

    feat: FEC-4 | Created release notes script

--- .github/release_notes.sh
+++ .github/release_notes.sh
@@ -1,11 +1,36 @@
-release_and_tag() {
+#echo $PREV_TAG
+#echo $NEW_TAG
+#echo $RELEASE_NOTES
 
-    curl \
-       -H "Accept: application/vnd.github+json" \
-       -H "Authorization: Bearer $TOKEN"\
-       -H "X-GitHub-Api-Version: 2022-11-28" \
-       https://api.github.com/repos/GouravSna/GH-TEST/pulls
-}
+nl=$'\n'
+touch $RELEASE_NOTES
+echo "## Changes from [$PREV_TAG](https://github.com/GouravSna/$REPO_NAME/releases/tag/$PREV_TAG)$nl" > $RELEASE_NOTES
 
-TOKEN=$TOKEN
-release_and_tag
+git log $PREV_TAG..HEAD --oneline --grep='(#' | cut -d' ' -f2- | while read -r line; do
+    echo "$line"
+
+    bugFixes="Bug Fixes"
+    newFeatures="New Features"
+
+    case $line in
+
+      *"fix"*)
+        grep -qF -- $bugFixes $RELEASE_NOTES || echo "### "$bugFixes$nl >> $RELEASE_NOTES
+        modifiedLine=$(echo "$line" | sed 's/fix://')
+sed -i '' '/'"$bugFixes"'/a\
+'"- $modifiedLine$nl"'' $RELEASE_NOTES
+        ;;
+
+      *"feat"*)
+        grep -qF -- $newFeatures $RELEASE_NOTES || echo "### "$newFeatures$nl >> $RELEASE_NOTES
+        modifiedLine=$(echo "$line" | sed 's/feat://')
+sed -i '' '/'"$newFeatures"'/a\
+'"- $modifiedLine$nl"'' $RELEASE_NOTES
+        ;;
+
+      *)
+        echo "### Additional Changes:" >> $RELEASE_NOTES
+        echo "$line$nl" >> $RELEASE_NOTES
+        ;;
+    esac
+done

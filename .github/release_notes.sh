528f265cb8ffcd495d1eaa6b939226fde9505abd
Gourav Saxena <gourav.saxena@kaltura.com>
2023-01-03 13:10:53 +0530

    - Fixed the `sed` commands

--- .github/release_notes.sh
+++ .github/release_notes.sh
@@ -16,20 +16,20 @@ git log $PREV_TAG..HEAD --oneline --grep='(#' | cut -d' ' -f2- | while read -r l
     if [[ "$line" == "fix"* || "$line" == "fix(FEC-"* ]]; then
 
       grep -qF -- $bugFixes $RELEASE_NOTES || echo "### "$bugFixes$nl >> $RELEASE_NOTES
-              modifiedLine=$(echo "$line" | sed 's/fix://')
+              modifiedLine=$(echo "$line" | sed 's/fix://' | sed 's/fix//' | sed 's|(\(FEC-[^)]*\))|\1|')
 sed -i '/'"$bugFixes"'/a\
 '"- $modifiedLine$nl"'' $RELEASE_NOTES
 
     elif [[ "$line" == "feat"* || "$line" == "feat(FEC-"* ]]; then
 
       grep -qF -- $newFeatures $RELEASE_NOTES || echo "### "$newFeatures$nl >> $RELEASE_NOTES
-              modifiedLine=$(echo "$line" | sed 's/feat://')
+              modifiedLine=$(echo "$line" | sed 's/feat://' | sed 's/feat//' | sed 's|(\(FEC-[^)]*\))|\1|')
 sed -i '/'"$newFeatures"'/a\
 '"- $modifiedLine$nl"'' $RELEASE_NOTES
 
     else
       grep -qF -- $moreChanges $RELEASE_NOTES || echo "### "$moreChanges$nl >> $RELEASE_NOTES
-      echo "$line$nl" >> $RELEASE_NOTES
+      echo "- $line$nl" >> $RELEASE_NOTES
 
     fi
 done

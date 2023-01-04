276bd33fa2180ab4ff1daffc23431c81de146f22
Gourav Saxena <gourav.saxena@kaltura.com>
2023-01-04 11:43:02 +0530

    - If there is no PR then add something else to release notes

--- .github/release_notes.sh
+++ .github/release_notes.sh
@@ -21,33 +21,38 @@ nl=$'\n'
 touch $RELEASE_NOTES
 echo "## Changes from [$PREV_TAG](https://github.com/GouravSna/$REPO_NAME/releases/tag/$PREV_TAG)$nl" > $RELEASE_NOTES
 
-git log $PREV_TAG..HEAD --oneline --grep='(#' | cut -d' ' -f2- | while read -r line; do
-    echo "$line"
+resultedLine=$(git log $PREV_TAG..HEAD --oneline --grep='(#' | cut -d' ' -f2-)
+if [[ ! -n "$resultedLine" ]]; then
+  echo "### Plugin Playkit Support$nl v$NEW_VERSION" >> $RELEASE_NOTES
+else
+  git log $PREV_TAG..HEAD --oneline --grep='(#' | cut -d' ' -f2- | while read -r line; do
+      echo "$line"
 
-    bugFixes="Bug Fixes"
-    newFeatures="New Features"
-    moreChanges="More Changes"
+      bugFixes="Bug Fixes"
+      newFeatures="New Features"
+      moreChanges="More Changes"
 
-    if [[ "$line" == "fix"* || "$line" == "fix(FEC-"* || "$line" == "fix (FEC-"* ]]; then
+      if [[ "$line" == "fix"* || "$line" == "fix(FEC-"* || "$line" == "fix (FEC-"* ]]; then
 
-      grep -qF -- $bugFixes $RELEASE_NOTES || echo "### "$bugFixes$nl >> $RELEASE_NOTES
-              modifiedLine=$(echo "$line" | sed 's/fix://' | sed 's/fix//' | sed 's|(\(FEC-[^)]*\))|\1|')
+        grep -qF -- $bugFixes $RELEASE_NOTES || echo "### "$bugFixes$nl >> $RELEASE_NOTES
+                modifiedLine=$(echo "$line" | sed 's/fix://' | sed 's/fix//' | sed 's|(\(FEC-[^)]*\))|\1|')
 sed -i '/'"$bugFixes"'/a\
 '"- $modifiedLine$nl"'' $RELEASE_NOTES
 
-    elif [[ "$line" == "feat"* || "$line" == "feat(FEC-"* || "$line" == "feat (FEC-"* ]]; then
+      elif [[ "$line" == "feat"* || "$line" == "feat(FEC-"* || "$line" == "feat (FEC-"* ]]; then
 
-      grep -qF -- $newFeatures $RELEASE_NOTES || echo "### "$newFeatures$nl >> $RELEASE_NOTES
-              modifiedLine=$(echo "$line" | sed 's/feat://' | sed 's/feat//' | sed 's|(\(FEC-[^)]*\))|\1|')
+        grep -qF -- $newFeatures $RELEASE_NOTES || echo "### "$newFeatures$nl >> $RELEASE_NOTES
+                modifiedLine=$(echo "$line" | sed 's/feat://' | sed 's/feat//' | sed 's|(\(FEC-[^)]*\))|\1|')
 sed -i '/'"$newFeatures"'/a\
 '"- $modifiedLine$nl"'' $RELEASE_NOTES
 
-    else
-      grep -qF -- $moreChanges $RELEASE_NOTES || echo "### "$moreChanges$nl >> $RELEASE_NOTES
-      echo "- $line$nl" >> $RELEASE_NOTES
+      else
+        grep -qF -- $moreChanges $RELEASE_NOTES || echo "### "$moreChanges$nl >> $RELEASE_NOTES
+        echo "- $line$nl" >> $RELEASE_NOTES
 
-    fi
-done
+      fi
+  done
+fi
 
 echo "### Gradle" >> $RELEASE_NOTES
 echo "$nl* \`implementation 'com.kaltura.netkit:netkit-core:$NEW_VERSION"\'\` >> $RELEASE_NOTES
